9f3a223a72f845dc4419e8841466ee46abd69b18
Rakesh Prajapati <rakesh.prajapati@theadroit.in>
2023-07-10 10:09:42 +0530

    some changes

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,9 @@
 var User = require('../models/user.js')
 const multer = require('multer');
 
+const upload = multer({ dest: 'uploads/' });
 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); 
-    },
-    filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-    },
-  });
-  
-const upload = multer({ storage: storage });
 
 const getAllUsers = async (req , res) =>{
     User.find({}).exec()
@@ -32,27 +22,14 @@ const addUser = async (req ,res) =>{
 
 };
 
-const uploadfile = async (res ,req) => {
-    if (!req.file) {
-        return res.status(400).send('No image uploaded.');
-      }
-    
-      const imagePath = req.file.path;
-      // Process the image or save its path to the database
-    
-      res.send('Image uploaded successfully.');
-};
+
 
 const createUser = async (req , res) => {
-    if (req.file) {
-        console.log('File uploaded:', req.file);
-        // Process the file as needed (e.g., save it to the database)
-    }
+    
     var user = new User({
         name:req.body.name,
         email:req.body.email,
         password:req.body.password,
-        image:filename,
     });
 
     user.save()
@@ -80,7 +57,7 @@ const updateUser = async (req ,res) => {
     res.redirect('/');
 };
 
-const deleteUser = async (res,req) => {
+const removeUser = async (req , res) => {
     await User.findByIdAndDelete(req.params.id);
     res.redirect('/');
 
@@ -88,4 +65,4 @@ const deleteUser = async (res,req) => {
 
 
 
-module.exports = {getAllUsers,addUser,createUser,editUser,updateUser,deleteUser,uploadfile};
+module.exports = {getAllUsers,addUser,createUser,editUser,updateUser,removeUser};
